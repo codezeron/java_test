b4051befb2a81c4ec378c3cd73ce7f86a7e6ad0a
codezeron <enzoyh@gmail.com>
2024-07-19 14:57:37 -0300

    feat: atualizando dependências; criando e populando as tabelas

---
+++ src/main/resources/db.migrations/populateC&M.sql
@@ -0,0 +1,12 @@
+INSERT INTO CLIENT (id, NAME) VALUES
+(1, 'Apple Inc.'),
+(2, 'Google'),
+(3, 'Microsoft'),
+(4, 'Tesla'),
+(5, 'SpaceX');
+
+INSERT INTO MODULE (id, NAME) VALUES
+(1, 'Financeiro'),
+(2, 'Vendas'),
+(3, 'Expedição'),
+(4, 'Foguetes');
