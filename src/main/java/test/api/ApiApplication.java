b902e24926dd1fd8cc943e4998d7f89cc98a90c9
alfaenzoh <enzoh@alfaconsultorias.com.br>
2024-08-02 15:51:04 -0300

    fix: arrumando os erros das dependencias e de importacao

--- src/main/java/test/api/ApiApplication.java
+++ src/main/java/test/api/ApiApplication.java
@@ -1,10 +1,8 @@
 package test.api;
 
-import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
